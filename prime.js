d4f55337f6135fc37730433d9de5fb1c7af6820d
Sourav Dhua <souravdhua1997@gmail.com>
2023-09-12 19:59:31 +0530

    added prime no code

---
+++ prime.js
@@ -0,0 +1,13 @@
+let num = 5 ;
+let count = 0 ; 
+for(let i = 2 ; i < num**(0.5) ; i++){
+	if(num % i == 0){
+		count ++ ;
+		}
+	}
+}
+if(count == 0){
+	console.log("prime number");
+}else{
+	console.log("composite number");
+}
